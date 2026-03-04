ac5f0507239c583b6568065042a307af3d62eb28
vikas <aiml9.vikas@gmail.com>
2026-03-04 16:31:00 +0530

    raw data push

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
+from src.datascience import logger
 def main():
     print("Hello from datascience-f!")
-
+    logger.info("welcome to data sciense first project")
 
 if __name__ == "__main__":
     main()
